10d5f9dcf79975dbe9c130ef53a404572b9ec9b4
megatiger <60770807+megatiger@users.noreply.github.com>
2020-06-19 23:14:48 +0900

    20200619 2314

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Movie App 2020
+
+React Js Fundamentals Course 2020
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+function Food({name, picture, rating}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>  
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
+
+const foodILike = [
+  {
+    id:1,
+    name: "김치",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/220px-Various_kimchi.jpg",
+    rating: 5,
+  },
+  {
+    id:2,
+    name: "삼겹살",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8f/%EC%82%BC%EA%B2%B9%EC%82%B4.jpg",
+    rating: 4.5,
+  },
+  {
+    id:3,
+    name: "비빕밥",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg",
+    rating: 4.3,
+  },
+  {
+    id:4,
+    name: "돈가스",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Dongaseu_4.jpg/220px-Dongaseu_4.jpg",
+    rating: 4.7,
+  },
+];
+
+
+
+function App() {
+  return (
+  <div>
+    {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
+  </div>
+  );
+}
+
 
 export default App;
