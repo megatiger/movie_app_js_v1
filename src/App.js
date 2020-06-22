46b2b48ebfc857853a63750c756e757574da500d
megatiger <60770807+megatiger@users.noreply.github.com>
2020-06-22 20:25:17 +0900

    20200622 1825

--- src/App.js
+++ src/App.js
@@ -1,59 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>  
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    }= await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  }
+
+  componentDidMount() {
+    // 영화 데이터 로딩 !
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>  
+        ) : (
+          <div className="movies">
+            { movies.map((movie) => (
+              <Movie 
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+            ))
+            }
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-
-const foodILike = [
-  {
-    id:1,
-    name: "김치",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Various_kimchi.jpg/220px-Various_kimchi.jpg",
-    rating: 5,
-  },
-  {
-    id:2,
-    name: "삼겹살",
-    image: "https://upload.wikimedia.org/wikipedia/commons/8/8f/%EC%82%BC%EA%B2%B9%EC%82%B4.jpg",
-    rating: 4.5,
-  },
-  {
-    id:3,
-    name: "비빕밥",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg",
-    rating: 4.3,
-  },
-  {
-    id:4,
-    name: "돈가스",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Dongaseu_4.jpg/220px-Dongaseu_4.jpg",
-    rating: 4.7,
-  },
-];
-
-
-
-function App() {
-  return (
-  <div>
-    {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
-  </div>
-  );
-}
-
 
 export default App;
